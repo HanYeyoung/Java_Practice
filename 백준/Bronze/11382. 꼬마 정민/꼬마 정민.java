557d2401edff23bd6ddaef02db5eda7da9fa972b
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-02 23:39:31 -0500

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        long A = scnr.nextLong();
+        long B = scnr.nextLong();
+        long C = scnr.nextLong();
+        
+        System.out.println(A + B + C);
+    }
+}
