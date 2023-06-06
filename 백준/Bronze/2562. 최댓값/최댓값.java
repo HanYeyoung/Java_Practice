b2270be6d09ece45b9bbc641ae4b815892486291
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-05 21:26:35 -0500

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int[] array = new int[9];
+        int max = array[0];
+        int index = 0;
+        
+        for (int i = 0 ; i < 9 ; i++) {
+            array[i] += scnr.nextInt();
+        }
+        
+        for (int i = 0 ; i < 9 ; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+
+    }
+}
