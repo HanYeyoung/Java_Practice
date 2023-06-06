585252fc5a219c62dca0f788b41eb615e2812a1f
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-06 15:13:45 -0500

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,23 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        int n = scnr.nextInt();
-        int[] array = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            array[i] = scnr.nextInt();
-        }
-        
-        int v = scnr.nextInt();
-        int count = 0;
-        
-        for (int i = 0 ; i < n ; i++) {
-            if (array[i] == v) {
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
+
+	public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     
+       int n = Integer.parseInt(br.readLine());
+       int[] array = new int[n];
+       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      
+       for (int i = 0 ; i < n ; i++) {
+           array[i] = Integer.parseInt(st.nextToken());
+       } 
+      
+       int v = Integer.parseInt(br.readLine());
+       int count = 0;
+      
+       for (int i = 0 ;i < n; i++) {
+           if (array[i] == v) {
+              count++;
+           }
+       }
+      
+       System.out.println(count);
+   }
 }
