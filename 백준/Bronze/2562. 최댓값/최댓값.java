3a05d897cf1a72577d64ab24c15176cf9ca61656
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-09 13:43:55 -0500

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
-        int[] array = new int[9];
-        int max = array[0];
-        int index = 0;
-        
-        for (int i = 0 ; i < 9 ; i++) {
+
+	public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int[] array = new int[9];
+       int max = array[0]; //initialize
+       int index = 0;
+
+       for (int i = 0 ; i < 9 ; i++) {
            array[i] = Integer.parseInt(br.readLine());
            if (array[i] > max) {
-                max = array[i];
-                index = i + 1;
-            }
-        }
-        System.out.println(max);
-        System.out.println(index);
-
-    }
+              max = array[i];
+              index = i + 1;
+           }
+       }
+      
+       System.out.println(max);
+       System.out.println(index);
+   }
 }
