818415114413ede4cef438454cc8ff982c6a5a28
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-13 15:29:16 -0500

    [Bronze II] Title: 바구니 뒤집기, Time: 152 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+   public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+       int N = Integer.parseInt(st.nextToken());
+       int M = Integer.parseInt(st.nextToken());
+       int[] array = new int[N];
+      
+       for (int i = 0; i < N; i++) {
+            array[i] = i + 1;
+        }
+      
+       for (int index = 0 ; index < M ; index++) {
+           int temp = 0;
+           st = new StringTokenizer(br.readLine(), " ");
+          
+           int i = Integer.parseInt(st.nextToken());
+           int j = Integer.parseInt(st.nextToken());
+          
+           while(i < j){
+              temp = array[i-1];
+              array[i-1] = array[j-1];
+              array[j-1] = temp;
+              i++;
+              j--;
+             
+           }
+       }
+       
+       br.close();
+      
+       for (int i = 0 ; i < array.length ; i++) {
+           System.out.print(array[i] + " ");
+       }
+   }
+}
