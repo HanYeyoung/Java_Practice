8b313f999d59e2772e3cfaa796205d5e88351c7d
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-18 15:24:08 -0500

    [Bronze V] Title: 행렬 덧셈, Time: 180 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,38 +1,37 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
       
-      int n = Integer.parseInt(st.nextToken());
-      int m = Integer.parseInt(st.nextToken());
-      
-      int[][] arr1 = new int[n][m];
-      int[][] arr2 = new int[n][m];
-      
-      for (int i = 0 ; i < n ; i++) {
-         st = new StringTokenizer(br.readLine(), " ");
-         for (int j = 0 ; j < m ; j++) {
-            arr1[i][j] = Integer.parseInt(st.nextToken());
-         }
-      }
-      
-      for (int i = 0 ; i < n ; i++) {
-         st = new StringTokenizer(br.readLine(), " ");
-         for (int j = 0 ; j < m ; j++) {
-            arr2[i][j] = Integer.parseInt(st.nextToken());
-         }
-      }
-      
-      for (int i = 0 ; i < n ; i++) {         
-         for (int j = 0 ; j < m ; j++) {
-            System.out.print(arr1[i][j] + arr2[i][j] + " ");
-         }
-         System.out.println();
-      }
-   }
+		int[][] arr1 = new int[n][m];
+		int[][] arr2 = new int[n][m];
+
+		for(int i = 0; i < n; i++){
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j = 0; j < m; j++){				
+				arr1[i][j] = Integer.parseInt(st.nextToken());		
+			}
+		}
+
+		for(int i = 0; i < n; i++){
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j = 0; j < m; j++){
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+				arr1[i][j] += arr2[i][j];
+				sb.append(arr1[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
 }
