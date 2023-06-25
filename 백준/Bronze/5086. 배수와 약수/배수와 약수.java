3a7c8ec5b67a3f5dc64169d4b89a153722c34e05
HanYeyoung <134107088+HanYeyoung@users.noreply.github.com>
2023-06-25 01:35:05 -0500

    [Bronze III] Title: 배수와 약수, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,23 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-   public static void main(String[] args) {
-      
-      Scanner scnr = new Scanner(System.in);
-      while (true) {
-          int a = scnr.nextInt();
-          int b = scnr.nextInt();
-      
-          if ((a == 0) && (b == 0)) {
-              break;
-          }
-          if (b % a == 0) {
-              System.out.println("factor");
-          } else if (a % b == 0) {
-              System.out.println("multiple");
-          } else {
-              System.out.println("neither");
-          }
-      }  
-   }
+    public static void main(String[] args) throws IOException  {
+        String factor = "factor\n";
+        String multiple = "multiple\n";
+        String neither = "neither\n";
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (a == 0 && b == 0) {
+                break;
+            }
+            if (b % a == 0) {
+                sb.append(factor);
+            } else if (a % b == 0) {
+                sb.append(multiple);
+            } else {
+                sb.append(neither);
+            }
+        }
+        System.out.print(sb);
+    }
 }
