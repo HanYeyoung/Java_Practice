79e7c567e45b7037c833332396ad14915b4d9a44
Adelia Han <134107088+HanYeyoung@users.noreply.github.com>
2024-02-26 21:37:31 -0600

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,31 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+    
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> hs = new HashSet<Integer>();
         
-        int[] array = new int[10];
-        int count = 0;
-       
-        for (int i = 0; i < 10; i++) {
-            array[i] = Integer.parseInt(br.readLine()) % 42;
+        for(int i=0; i<10; i++) {
+        	hs.add(Integer.parseInt(br.readLine()) % 42);
         }
-       
-        for (int i = 0; i < 10; i++) {
-            int temp = 0;
-            for (int j = i+1; j < 10; j++) {
-                if (array[i] == array[j]) {
-                    temp++;
-                }
-            }
-            if (temp == 0) {
-                count++;
-            }
-        }
-       
-        System.out.println(count);
+        br.close();
+        System.out.print(hs.size());
     }
 }
